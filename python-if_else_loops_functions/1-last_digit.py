a31335c9fc8cdb9b890ffbe4ea20e3002430dfa6
juan hp <fernan2lopezkto@gmail.com>
2022-09-05 21:20:52 -0300

    fix task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,4 +2,11 @@
 import random
 from tkinter import commondialog
 number = random.randint(-10000, 10000)
-aca va todo el commondialog
+str = str(number)
+intero = int(str[-1])
+if intero > 5:
+    print(f"Last digit of {number} is {str[-1]} and is greater than 5")
+elif intero == 0:
+    print(f"Last digit of {number} is {str[-1]} and is 0")
+else:
+    print(f"Last digit of {number} is {str[-1]} and is less than 6 and not 0")
