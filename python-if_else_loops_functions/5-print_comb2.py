fbf5c2ab148b06520644d001372b92a043d9c472
Juan Rey <5079@holbertonschool.com>
2022-09-08 22:32:20 -0300

    fix 5 task

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i < 10:
-        print("0", end="")
-
-    print("{}".format(i), end=", ")
+    print("{:02}".format(i), end="")
+    if i < 99:
+        print(", ", end="")
