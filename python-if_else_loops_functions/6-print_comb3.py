d150031432fbe94a8a5060efaf3845097358eb43
Juan Rey <5079@holbertonschool.com>
2022-09-09 15:59:29 -0300

    fix task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for a in range(10):
     for b in range (10):
         if a * 10 + b < b * 10 + a:
-            print("{}{}".format(a, b), end="")
+            print( "{}{}".format(a, b), end="")
             if a * 10 + b < 89:
                 print(end=", ")
