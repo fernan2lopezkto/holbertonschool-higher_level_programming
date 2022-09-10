32b032730e004808c7b64a10c3b8d5423b8106e0
juan <fernan2lopezkto@gmail.com>
2022-09-10 10:57:35 -0700

    yesss 7 fix

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-
-    if ord('c') < 124:
-        if ord('c') > 96:
-            return True
-        else:
-            return False
+    if ord(c) < 123 and ord(c) > 94:
+        return True
+    else:
+        return False
 
