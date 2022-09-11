150c2e268f80a2816d385b141946b7d9dfcd732d
Juan Rey <5079@holbertonschool.com>
2022-09-11 15:53:29 -0300

    the task 11 files fix style

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,13 +2,10 @@
 def pow(a, b):
     r = a
     n = b * -1
-
     if b == 0:
         return 1
-
     if b == 1:
         return a
-
     if b < 0:
         if b == -2:
             r = r * a
@@ -16,9 +13,6 @@ def pow(a, b):
         for i in range(n - 1):
             r = r * a
         r = 1 / r
-
-    
     for i in range(b - 1):
         r = r * a
-        
     return r
