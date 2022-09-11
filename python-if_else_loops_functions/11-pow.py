3af9139c96df0bcc5a83636ed6e694620d15a2db
juan <fernan2lopezkto@gmail.com>
2022-09-11 09:40:01 -0700

    ad files

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def pow(a, b):
+    r = a
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    elif b < 0:
+        b = - b
+        for i in range(b)
+            r = r / a
+    else:
+        for i in range(b - 1):
+            r = r * a
+        
+    return r
