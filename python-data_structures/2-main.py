3b1ab23f7fc1cc2b1b6c4546770af3bc642a6a8e
juan hp <fernan2lopezkto@gmail.com>
2022-09-13 20:29:09 -0300

    add files to task 2

--- python-data_structures/2-main.py
+++ python-data_structures/2-main.py
@@ -2,9 +2,16 @@
 replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
+idx = 5
 new_element = 9
 new_list = replace_in_list(my_list, idx, new_element)
 
 print(new_list)
+print(my_list)
+
+idx = 2
+new_element = 7
+new_list2 = replace_in_list(my_list, idx, new_element)
+
+print(new_list2)
 print(my_list)
