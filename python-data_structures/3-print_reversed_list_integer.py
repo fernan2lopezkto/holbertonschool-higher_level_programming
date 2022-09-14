a3fea49f265dbf03f21260cf23c726bbb28aa0a5
juan hp <fernan2lopezkto@gmail.com>
2022-09-13 21:00:49 -0300

    fix task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+from audioop import reverse
+
+
 def print_reversed_list_integer(my_list=[]):
-        print("{:d}".format(my_list[::-1]))
+    idx = len(my_list) - 1
+    while idx >= 0:
+        print("{:d}".format(my_list[idx]))
+        idx = idx - 1
