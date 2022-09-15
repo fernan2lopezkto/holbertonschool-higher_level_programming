bab50a62ffd4aba641435fa71972148edd76e782
Juan Rey <5079@holbertonschool.com>
2022-09-15 11:27:45 -0300

    fix task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     result_list = []
 
-    for i in my_list:
+    for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             result_list.append(True)
         else:
