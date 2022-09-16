e656d30f5857b26e933a7e569c904abfe7d8c271
John King <fernan2lopezkto@gmail.com>
2022-09-16 17:22:44 -0300

    ok fix task 0

--- python-more_data_structures/0-main.py
+++ python-more_data_structures/0-main.py
@@ -10,3 +10,21 @@ matrix = [
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
 print(matrix)
+
+matrix2 = [[1, 2], [4, 5], [7, 8]]
+
+new_matrix2 = square_matrix_simple(matrix2)
+print(new_matrix2)
+print(matrix2)
+
+matrixz = [[]]
+
+new_matrixz = square_matrix_simple(matrixz)
+print(new_matrixz)
+print(matrixz)
+
+matrixx = [[1], [2], [3], [4]]
+
+new_matrixx = square_matrix_simple(matrixx)
+print(new_matrixx)
+print(matrixx)
