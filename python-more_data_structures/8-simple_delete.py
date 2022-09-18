da2046e6b51b4c98bfc62eee1c922c4b1d9b6453
juan hp <fernan2lopezkto@gmail.com>
2022-09-18 17:02:50 -0300

    task 8

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    there_is = False
+    for clave in a_dictionary:
+        if clave == key:
+            there_is = True
+    if there_is == True:
+        a_dictionary.pop(key)
+    return a_dictionary
