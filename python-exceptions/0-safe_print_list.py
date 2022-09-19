328f8b33ac223e7b6b77c1a9b1f968db894d4f55
juan hp <fernan2lopezkto@gmail.com>
2022-09-18 21:34:22 -0300

    add task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from typing import Counter
+
+
+def safe_print_list(my_list=[], x=0):
+    Counter = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            Counter += 1
+        except:
+            pass
+    print()
+    return Counter
