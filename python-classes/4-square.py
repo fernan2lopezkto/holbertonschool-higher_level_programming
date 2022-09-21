3d48e4fc3395eb0e508f5beaf4ac294e97d93d2e
juan hp <fernan2lopezkto@gmail.com>
2022-09-21 10:57:35 -0300

    add

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""an other Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """ size = int ? """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        if not int(size) == size:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """Initialize a new Square.
+        size (int): The size of the new square"""
         self.__size = size
 
-    def area(self):
-        return self.__size ** 2
-
+    @property
     def size(self):
+        """Get/set the current size of the square."""
         return self.__size
-    
+
+    @size.setter
     def size(self, value):
-        if not int(value) == value:
+        """Sets the size to a value."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
-
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
