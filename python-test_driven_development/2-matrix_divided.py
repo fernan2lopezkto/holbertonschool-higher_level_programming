530894cc66d59d1200c59c132384fa669af6bbf4
John King <fernan2lopezkto@gmail.com>
2022-09-22 19:36:49 -0300

    test avance in task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,16 +11,21 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     the_matrix = []
+    textE = "matrix must be a matrix (list of lists) of integers/floats"
     a = len(matrix[0])
-    for columna in range(len(matrix)):
-        if len(matrix[columna]) != a:
+    for c in range(len(matrix)):
+        if len(matrix[c]) != a:
             raise TypeError("Each row of the matrix must have the same size")
         nl = []
         for fila in range(len(matrix[0])):
-            if type(matrix[columna][fila]) == int or type(matrix[columna][fila]) == float:
-                nl.append(round(matrix[columna][fila] / div, 2))
+            if type(matrix[c][fila]) == int or type(matrix[c][fila]) == float:
+                nl.append(round(matrix[c][fila] / div, 2))
             else:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(textE)
         the_matrix.append(nl)
 
     return the_matrix
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
