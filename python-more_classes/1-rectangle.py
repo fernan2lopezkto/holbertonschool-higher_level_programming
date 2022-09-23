cb7992d680490fc67b172e2dc27d4e6323f8d1ef
John King <fernan2lopezkto@gmail.com>
2022-09-23 20:56:39 -0300

    ok

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         return self.__width
@@ -19,7 +20,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
