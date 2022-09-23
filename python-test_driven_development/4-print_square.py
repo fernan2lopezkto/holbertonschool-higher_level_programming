c8c2bafe53ed2efd5154df67de3c1bc624ddec0d
John King <fernan2lopezkto@gmail.com>
2022-09-23 13:36:47 -0300

    fix to test test test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,3 +15,7 @@ def print_square(size):
         for y in range(size):
             print("#", end="")
         print()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
