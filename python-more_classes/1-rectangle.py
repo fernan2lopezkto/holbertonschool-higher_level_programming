64063e81a36dbaa1d050558f2b613ed1f9115a2c
John King <fernan2lopezkto@gmail.com>
2022-09-23 20:51:00 -0300

    fix task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TabError("width must be an integer")
+            raise TabError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
