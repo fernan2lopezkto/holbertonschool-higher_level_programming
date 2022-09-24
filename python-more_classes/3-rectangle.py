3eebf945ba379d247b1dd819354b96e779217c82
John King <fernan2lopezkto@gmail.com>
2022-09-24 00:45:40 -0300

    task 3 files

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" 2-rectangle module """
+
+
+class Rectangle:
+    """ Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value:
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+        else:
+            self.__width = 0
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height < 1 or self.__width < 1:
+            return 0
+        else:
+            return (self.__height + self.__width) * 2
+
+
+    def __str__(self):
+        a = "#" * self.width
+        for h in range(0, self.height - 1):
+            for w in range(0, self.width):
+                print("#", end="")
+            print()
+        return a
+
+
