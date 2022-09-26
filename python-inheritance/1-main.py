65675a2599785a03bdcfc943590703549f97c8cc
juan hp <fernan2lopezkto@gmail.com>
2022-09-26 19:03:37 -0300

    try fix task 1 herrencia

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -2,13 +2,13 @@
 MyList = __import__('1-my_list').MyList
 
 li = MyList()
+li.append(1)
+li.append(3)
+li.append(2)
 
-
-li.append("b")
-li.append("a")
-li.append("m")
 
 
 print(li)
 
-li.print_sorted()
+a = li.print_sorted()
+print(a)
