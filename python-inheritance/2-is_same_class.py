01ddfc80456933b18c2807f9d59e37d5e2df3c68
juan hp <fernan2lopezkto@gmail.com>
2022-09-26 19:21:39 -0300

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 2-is_same_name module """
+
+
+def is_same_class(obj, a_class):
+    """ function """
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
