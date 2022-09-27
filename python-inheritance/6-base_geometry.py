b6efb71414d3275c7836971cc0f397b255917f31
juan hp <fernan2lopezkto@gmail.com>
2022-09-27 11:02:20 -0300

    task 5 trying

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 5-base_geometry """
+
+
+from logging import exception
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
