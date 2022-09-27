86fc2f7519a3960da15baaa2e3b75bbd3812f142
juan hp <fernan2lopezkto@gmail.com>
2022-09-27 12:40:15 -0300

    fix test task 7

--- python-inheritance/6-main.py
+++ python-inheritance/6-main.py
@@ -3,7 +3,5 @@ BaseGeometry = __import__('6-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-try:
-    print(bg.area())
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+
+print(bg.area())
