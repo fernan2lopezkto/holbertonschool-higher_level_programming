c7cacd4e2037d2f12e5ac53434ba20ba593d82c1
juan hp <fernan2lopezkto@gmail.com>
2022-09-27 20:46:45 -0300

    try any things

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,10 +2,10 @@
 """ Module 9-rectangle """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('7-base_geometry').BaseGeometry
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     """ This is a class """
     def __init__(self, size):
         self.integer_validator("size", size)
