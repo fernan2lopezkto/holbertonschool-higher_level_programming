9d6ffbb671e87b5f3c245e9088c0cba895acac0b
juan <fernan2lopezkto@gmail.com>
2022-09-28 12:32:31 -0700

    add task 3 -3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 """
-Module
-""" 
-  
-  
-import json 
-  
-  
-def to_json_string(my_obj): 
+module
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
     """
-    string to json
-    """ 
-    return json.dumps(my_obj)
+    function
+    """
+
+    return json.dumps(my_obj)
 
