d513f07ce8783dc296753c52d40f67965a868558
juan hp <fernan2lopezkto@gmail.com>
2022-09-28 19:35:31 -0300

    task 5 files

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pithon3
+""" 5-save_to_json_file module """
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ save to json file """
+
+    with open(filename, "w+") as f:
+        f.write(json.dumps(my_obj))
