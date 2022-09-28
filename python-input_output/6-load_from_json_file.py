b358cd14aec9f9bd589e78285a5d2511b42b6ad6
juan hp <fernan2lopezkto@gmail.com>
2022-09-28 20:24:29 -0300

    try task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 6-load_from_json_file """
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ function """
+
+    with open(filename, "r+") as f:
+        return json.load(f)
