628bce0e35b1bbda946aa9cbe6e31959a65a98fa
juan hp <fernan2lopezkto@gmail.com>
2022-09-28 08:29:06 -0300

    add documentations

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" 0-read file module """
+
+
 def read_file(filename=""):
+    """
+    function to read and print file lines
+    """
     with open(filename) as f:
         read_data = f.read()
 
