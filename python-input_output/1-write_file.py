d1167797553c593e1828bb0c01cb6a4361868805
juan hp <fernan2lopezkto@gmail.com>
2022-09-28 08:56:34 -0300

    ok

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     """
     function to write in file
     """
-    with  open(filename, "w") as f:
+    with open(filename, "w") as f:
         n_l = f.write(text)
 
     return n_l
