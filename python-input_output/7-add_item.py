1fae53b9f149f21afc1578313648217232ad0fff
juan hp <fernan2lopezkto@gmail.com>
2022-09-28 21:25:37 -0300

    opitinging

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,18 @@ import sys
 
 
 """ import any modules """
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 """ this is an onli simple script """
 
-for i in range(0, len(sys.argv)):
-    with open("add_item.json", "a") as f:
-        f.write(json.dumps(sys.argv[i]))
+list_python = load_from_json_file("add_item.json")
+""" convert the json a python list """
+
+for i in range(1, len(sys.argv)):
+    """ append values in arguments """
+    list_python.append(sys.argv[i])
+
+save_to_json_file(list_python, "add_item.json")
+""" save new file whit new values """
