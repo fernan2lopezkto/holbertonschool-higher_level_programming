cd5c54f2431a380832becef168b6202f9632a383
juan <fernan2lopezkto@gmail.com>
2022-09-28 19:53:42 -0700

    try 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-""" module """
+"""Add item module"""
 
 
-import sys
+import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-list_python = load_from_json_file("add_item.json")
+def main():
 
-for i in range(1, len(sys.argv)):
-    list_python.append(sys.argv[i])
+    items_list = []
 
-save_to_json_file(list_python, "add_item.json")
+    try:
+        items_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        pass
+    for i in range(1, len(sys.argv)):
+        items_list.append(sys.argv[i])
+
+    save_to_json_file(items_list, 'add_item.json')
+
+if __name__ == '__main__':
+    main()
