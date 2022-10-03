6f770b25eacd016ce78c9b47103d195ab632cd61
John King <fernan2lopezkto@gmail.com>
2022-10-03 12:01:00 -0300

    jljljlij

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor"""
+
         super().__init__(id)
         self.__width = width
         self.__height = height
