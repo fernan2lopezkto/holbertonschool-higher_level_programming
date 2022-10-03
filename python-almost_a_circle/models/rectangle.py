7389666010473ba5d7ad07a9277da4eb5b41f761
John King <fernan2lopezkto@gmail.com>
2022-10-03 12:53:04 -0300

    remove underscore

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ constructor function """
 
-        self.__width = width
+        self.width = width
         self.__height = height
         self.__x = x
         self.__y = y
