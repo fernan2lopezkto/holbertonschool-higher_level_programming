45e1b76a76857b6c5439d7232c97f13000598274
John King <fernan2lopezkto@gmail.com>
2022-10-03 14:27:59 -0300

    add xy func error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -51,33 +51,47 @@ class Rectangle(Base):
     def width(self, value):
         """ set width """
         type_error("width", value)
+        wh("width", value)
         self.__width = value
 
     @height.setter
     def height(self, value):
         """ set height """
         type_error("height", value)
+        wh("height", value)
         self.__height = value
 
     @x.setter
     def x(self, value):
         """ set x """
         type_error("x", value)
+        xy("x", value)
         self.__x = value
 
     @y.setter
     def y(self, value):
         """ set y """
         type_error("y", value)
+        xy("y", value)
         self.__y = value
 
-    """ 
-    ---------------
-    -- to errors --
-    ---------------
-    """
+""" 
+---------------
+-- to errors --
+---------------
+"""
 
 def type_error(name, value2):
     """ chek type """
     if type(value2) != int:
         raise TypeError(f"{name} must be an integer")
+
+def wh(name, value3):
+    """ check value of width and height """
+    if value3 < 0:
+        raise ValueError(f"{name} must be > 0")
+
+def xy(name, value4):
+    """ check value of x and y """
+    if value4 <= 0:
+        raise ValueError(f"{name} must be >= 0")
