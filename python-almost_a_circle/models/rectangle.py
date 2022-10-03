ae9734931585d550cd0b1470e1e27a9b8b4baf51
John King <fernan2lopezkto@gmail.com>
2022-10-03 12:33:25 -0300

    add basic self property

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,52 +1,16 @@
 #!/usr/bin/python3
-"""Module Rectangle"""
+""" rectangle module """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    Class Rectangle
-    """
+    """ Rectangle class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Constructor
-        """
+        """ constructor function """
 
-        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-
-def width(self):
-    """ ok """
-    return __width
-
-def width(self, value):
-    """ ok """
-    self.__width = value
-
-def height(self):
-    """ ok """
-    return self.__height
-
-def height(self, value):
-    """ ok """
-    self.__height = value
-
-def x(self):
-    """ ok """
-    return __x
-
-def x(self, value):
-    """ ok """
-    self.__x = value
-
-def y(self):
-    """ ok """
-    return __y
-
-def y(self, value):
-    """ ok """
-    self.__y = value
+        super().__init__(id)
