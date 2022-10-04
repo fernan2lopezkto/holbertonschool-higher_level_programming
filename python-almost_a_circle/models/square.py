5eb90eda09bbc7a8e9fb39cd1e884ca83b5f1d91
John King <fernan2lopezkto@gmail.com>
2022-10-04 18:15:30 -0300

    wrs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,10 +21,25 @@ class Square(Rectangle):
     def size(self, value):
         """ use width and height property """
         self.width = value
-        self.height = value 
+        self.height = value
 
     def __str__(self):
         """ whow width and heath """
         a = f"[Square] ({self.id}) {self.x}/{self.y}"
         b = f" - {self.width}"
         return a + b
+
+    def update(self, *args, **kwargs):
+        """ update function """
+        item_needed = ["id", "size", "x", "y"]
+        if type(args) == list:
+            for item in range(len(args)):
+                if item == 1:
+                    setattr(self, width, args[item])
+                    setattr(self, height, args[item])
+                else:
+                    setattr(self, item_needed[item], args[item])
+                
+        else:
+            for key in kwargs:
+                setattr(self, key, kwargs[key])
