0c36f5e7ad4c00fa285e433e3861d6abeda8a386
John King <fernan2lopezkto@gmail.com>
2022-10-04 17:08:56 -0300

    add task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+square module from rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class Square that inherits from Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """class constructor"""
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """__str__ method should return 
+        [Square] (<id>) <x>/<y> - <size> """
+        return f"[Square] ({self.id}) {self.x}/{self.height} - {self.width}"
