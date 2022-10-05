91f6ab017ff997ccc8b60e0e7999a7cce4a521b1
John King <fernan2lopezkto@gmail.com>
2022-10-05 18:23:22 -0300

    lol

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,12 +20,19 @@ class Base:
             self.id = id
 
     def to_json_string(list_dictionaries):
-        """ list to json
+        """ list to json strings
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             a = json.dumps(list_dictionaries)
         return a
+
+    def save_to_file(cls, ):
+        """ from list to json file
+        """
+        with open(f'{self.__class__.__name__}.json', 'w') as outfile:
+            json.dump(letters, outfile)
+
 
 Base.to_json_string = staticmethod(Base.to_json_string)
