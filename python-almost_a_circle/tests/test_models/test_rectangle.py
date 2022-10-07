271fedde7e20f48440dd15c05e71b274635d3108
John King <fernan2lopezkto@gmail.com>
2022-10-07 13:40:11 -0300

    get changes

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,7 +11,7 @@ class Test_rectangle(unittest.TestCase):
         result = Rectangle(1, 2)
         self.assertEqual(result.width, 1)
         self.assertEqual(result.height, 2)
-
+ 
     def test_more_argument(self):
         result = Rectangle(1, 2, 3)
         self.assertEqual(result.width, 1)
@@ -29,13 +29,41 @@ class Test_rectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             r = Rectangle("1", 2)
 
-    def test_string_other_arg(self):
+    def test_string_second_arg(self):
         with self.assertRaises(TypeError):
             r = Rectangle(1, "2")
 
     def test_string_other_arg(self):
         with self.assertRaises(TypeError):
             r = Rectangle(1, 2, "3")
+
+    def test_string_arg_4(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle(1, 2, 3, "4")
+
+    def test_negative_arg_0(self):
+        with self.assertRaises(ValueError):
+            r = Rectangle(-1, 2)
+
+    def test_negative_arg_1(self):
+        with self.assertRaises(ValueError):
+            r = Rectangle(1, -2)
+
+    def test_zero_arg_0(self):
+        with self.assertRaises(ValueError):
+            r = Rectangle(0, 2)
+
+    def test_zero_arg_1(self):
+        with self.assertRaises(ValueError):
+            r = Rectangle(1, 0)
+
+    def test_negative_arg_2(self):
+        with self.assertRaises(ValueError):
+            r = Rectangle(1, 2, -3)
+
+    def test_area(self):
+        e = Rectangle(5, 5)
+        self.assertEqual(e.area(), 25)
 
 if __name__ == '__main__':
     unittest.main()
