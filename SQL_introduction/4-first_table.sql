ede07a57bbb24df571e4fdcfbfda8f2013c8e792
John King <fernan2lopezkto@gmail.com>
2022-11-04 17:52:59 -0300

    T4 NOT FUNCTIONAL

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Write a script that creates a table called first_table in the current database in your MySQL server.
+-- task 4
+
+CREATE DATABASE IF NOT EXISTS;
+USE DATABASE;
+ALTER TABLE ADD ()
