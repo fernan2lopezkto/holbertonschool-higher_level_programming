3a2651ffddd52ea57a1758f64da0b094b3986e6c
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-06 20:58:59 -0300

    TASK 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- create new user
+-- task 1
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
