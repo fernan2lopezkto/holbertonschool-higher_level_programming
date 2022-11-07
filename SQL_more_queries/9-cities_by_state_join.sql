5f24fcde6b4e8b68f4306bd6f0ce300fc85ef202
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-06 22:29:50 -0300

    equal id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- 9-cities_by_state_join.sql
 
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states on states.id = cities.state_id ORDER BY cities.id;
