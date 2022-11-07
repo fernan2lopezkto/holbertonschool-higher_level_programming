c0b439cb2d8429449138e1eea134304592b507b7
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-06 22:48:39 -0300

    ON

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- tv shows
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.genre_id = genre_id;
