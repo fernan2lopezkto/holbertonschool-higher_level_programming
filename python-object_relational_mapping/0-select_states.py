0c5804b055e784213777d6c5bff4ce292af58df0
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-12 16:20:49 -0300

    add if name is main

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,17 +6,18 @@ import MySQLdb
 from sys import argv
 
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    password=argv[2],
-    database=argv[3]
-    )
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+        )
 
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-result = cursor.fetchall()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cursor.fetchall()
 
-for row in result:
-    print(row)
+    for row in result:
+        print(row)
