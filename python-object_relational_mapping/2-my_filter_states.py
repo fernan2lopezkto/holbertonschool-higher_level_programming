2b7321713ed5856bc8c38fcd5756c5dfb77c1e1e
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-12 17:44:15 -0300

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Write a scriptss """
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+        )
+
+    name_argument = argv[4]
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = {} ORDER BY states.id".format(name_argument))
+    result = cursor.fetchall()
+
+    for row in result:
+        if row[1][0] == 'N':
+            print(row)
