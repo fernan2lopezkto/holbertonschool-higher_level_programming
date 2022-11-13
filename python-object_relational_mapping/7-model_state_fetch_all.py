58d66cee91182235ec7a02251544c600d7594aa3
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-13 15:25:35 -0300

    koko

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2],argv[3]), pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2],argv[3]),)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(engine)
