5f372dcf8a13cb0f88763b4dc30bbe950076cb64
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-12 21:01:08 -0300

    TRY

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -17,14 +17,20 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
+    c = "SELECT id FROM cities AS coso WHERE name = {}; ".format(argv[4])
+
+    a = "SELECT name FROM cities WHERE id = COSO;"
+
+    """
     select = "SELECT cities.name "
     fromm = "FROM cities "
     iner = "INNER JOIN states "
     onn = "ON cities.state_id = states.id "
     ordr = "ORDER BY cities.id;"
+    """
 
-    cursor.execute(select + fromm + iner + onn + ordr)
+    cursor.execute(c + a)
     result = cursor.fetchall()
 
     for row in result:
-        print(row, end="")
+        print(row[0], end="")
