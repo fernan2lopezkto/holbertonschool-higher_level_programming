dbcde2f186918b7a93489edd5e7281219e133ebf
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-13 20:43:23 -0300

    task 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""MODULE task 9"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
+
+
+Base = declarative_base()
+
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State(name = "Louisiana")
+    session.add(louisiana)
+
+    session.commit()
+
+    result = session.query(State.id).filter(
+        State.name.like("Louisiana")
+    ).first()
+
+    if result:
+        print(result.id)
+    else:
+        print("Not found")
+
+    session.close()
