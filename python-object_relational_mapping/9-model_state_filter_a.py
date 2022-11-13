8050e08470b6671d8097313f9a61d395fc1dc8f8
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-13 18:51:58 -0300

    %a%

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     session = Session()
 
     result = session.query(State).filter(
-        State.name.like('%a')
+        State.name.like('%a%')
     ).order_by(State.id)
 
     if result:
