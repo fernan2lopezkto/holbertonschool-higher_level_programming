b913b5b9316d5719df0380dbf9d6f1731b3ff09d
Juan Linux <fernan2lopezkto@gmail.com>
2022-11-13 23:27:54 -0300

    ddddd

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/python3
+"""MODULE task 9"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
+
+
+Base = declarative_base()
+
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(
+        State.id.like(2))
+
+    result.name = 'New Mexico'
+
+    session.add(result)
+
+    session.commit()
+    session.close()
