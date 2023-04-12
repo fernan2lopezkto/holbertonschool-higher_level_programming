de751c8dcf45c885d9c5f2b18bfff726e08c5bed
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-12 07:37:26 -0700

    add task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const frases = 'C is fun';
+if (process.argv[2]) {
+  process.argv[2] = parseInt(process.argv[2], 10);
+  while (process.argv[2] != 0) {
+    console.log(frases);
+    process.argv[2] = process.argv[2] - 1;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
