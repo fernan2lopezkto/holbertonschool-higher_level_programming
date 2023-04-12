08ff2d8c6e349bcc2b27ff302ab13bcf71bbb967
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-12 01:12:15 -0300

    add task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+var args = process.argv;
+if (args.length == 3){
+    console.log('Argument found');
+}
+else if (args.length > 3){
+    console.log('Arguments found');
+}
+else {
+    console.log('No argument');
+}
