413720ea4d969a1dbd6b2eb1a05734e80f544e70
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-12 08:44:18 -0700

    fix task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const frases = 'C is fun';
 if (process.argv[2]) {
   process.argv[2] = parseInt(process.argv[2], 10);
-  while (process.argv[2] != 0) {
+    while (process.argv[2] > 0) {
     console.log(frases);
     process.argv[2] = process.argv[2] - 1;
   }
