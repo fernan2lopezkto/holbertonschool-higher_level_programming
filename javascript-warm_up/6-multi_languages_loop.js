c41883b52db7a84e87a9643b7b48736334130d26
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-12 02:48:48 -0300

    add task 6 fixed

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const frases = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const frases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 frases.forEach(element => console.log(element));
