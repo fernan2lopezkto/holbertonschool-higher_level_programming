2281dae77f4205f545ecb33723bc54479907d4bc
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2023-04-12 15:06:21 -0300

    Actualizar 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const frases = 'X';
 if (process.argv[2]) {
   process.argv[2] = parseInt(process.argv[2], 10);
   while (process.argv[2] > 0) {
-    for (var i = 0; i < process.argv[2]; i ++) {
+    for (let i = 0; i < process.argv[2]; i ++) {
       console.log(frases);
       process.argv[2] = process.argv[2] - 1;
     }
