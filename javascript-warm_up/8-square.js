9118b68c360c4c5199a0fe6815c169415cfa7b6c
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-12 20:03:04 -0700

    gg

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 if (process.argv[2]) {
-  process.argv[2] = parseInt(process.argv[2], 10);
-  const stop = process.argv[2];
-  while (process.argv[2] > 0) {
-    let i = 0;
-    let eX = '';
-    while (i < stop) {
-      eX = eX + 'X';
-      i = i + 1;
+  if (isNaN(process.argv[2]) {
+    console.log('Missing size');
+  } else {
+    process.argv[2] = parseInt(process.argv[2], 10);
+    const stop = process.argv[2];
+    while (process.argv[2] > 0) {
+      let i = 0;
+      let eX = '';
+      while (i < stop) {
+        eX = eX + 'X';
+        i = i + 1;
+      }
+      console.log(eX);
+      process.argv[2] = process.argv[2] - 1;
     }
-    console.log(eX);
-    process.argv[2] = process.argv[2] - 1;
   }
-}
+} else {
 console.log('Missing size');
+}
