b80a24bcf53e5ac2ffdb90591441713310711a6b
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-12 18:21:49 -0700

    fix task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 if (process.argv[2]) {
-  process.argv[2] = parseInt(process.argv[2], 10);
+  let stop = process.argv[2] = parseInt(process.argv[2], 10);
   while (process.argv[2] > 0) {
-    let i = 0
-    let eX = ''
-    for (i < process.argv[2]) {
+    let i = 0;
+    let eX = '';
+    while (i < stop) {
       eX = eX + 'X';
-      console.log(ex);
-      i ++;
+      i = i + 1;
     }
+    console.log(eX);
+    process.argv[2] = process.argv[2] - 1;
   }
 } else {
   console.log('Missing size');
