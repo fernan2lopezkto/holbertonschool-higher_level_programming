8194671142adc76bec5162e0e6c36fdcde30a2bc
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-13 02:13:56 -0300

    add task 10 fixed

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-
-function factorialRecursivo (n) { 
-	if (n == 0){ 
-		return 1; 
+function factorialRecursivo (n) {
+	if (n === 0){
+		return 1;
 	}
-	return n * factorialRecursivo (n-1); 
+	return n * factorialRecursivo (n-1);
 }
-
 
 if (isNaN(process.argv[2])) {
   console.log(1);
