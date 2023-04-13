b92816f75dcddf9141f9eb0efe4d406805ff1fea
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-13 03:23:18 -0300

    add task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// variavles
+let argums = process.argv;
+
+// there is more of one arguments
+if (argums.length > 3) {
+  let bigbig = 0;
+  let secbig = 0;
+  for (i = 2; i < argums.length; i++) {
+    let nume = parseInt(argums[i]);
+    if (nume > bigbig && nume > secbig) {
+      secbig = bigbig;
+      bigbig = nume;
+    } else if (nume < bigbig && nume > secbig) {
+      secbig = nume;
+    }
+  }
+  console.log(secbig);
+} else {
+  console.log(0);
+}
