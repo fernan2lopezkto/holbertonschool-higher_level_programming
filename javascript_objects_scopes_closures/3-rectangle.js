07522a185432fa696ab5556ddc48494f9ef79816
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-14 01:56:16 -0300

    add task 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      let ast = '';
+      while (j < this.width) {
+        ast = ast + 'X';
+        j++;
+      }
+      console.log(ast)
+      i++;
+    };
+
+  };
+
+};
+module.exports = Rectangle;
