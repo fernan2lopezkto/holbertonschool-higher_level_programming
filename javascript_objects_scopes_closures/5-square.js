9d146578876454b3d10e20de794de7e8b19d3ca1
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-14 02:32:19 -0300

    add task 5

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      let ast = '';
+      while (j < this.width) {
+        ast = ast + 'X';
+        j++;
+      }
+      console.log(ast);
+      i++;
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
