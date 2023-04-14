b38fec5e12fa5f6fabd8e959ce50d278fdbff56d
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-14 10:13:32 -0700

    add

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * crear una lista con diccionarios adentro
+ * cada deccionario guarda <len de lista>:<value>
+ * un for para printear ky:value
+ */
+exports.logMe = function (item) {
+
+}
+
+//ouput format
+//<number arg already printed>: <current arg value>
