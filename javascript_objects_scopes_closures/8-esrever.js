abe25c0181ec3fc7d37702200c5334ed2e805ee8
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-14 09:15:59 -0700

    add task 8 fix

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  // return list.reverse();
+  const outp = [];
+  for (let i = 0; i < list.length; i++) {
+    outp.unshift(list[i]);
+  }
+  return outp;
 };
