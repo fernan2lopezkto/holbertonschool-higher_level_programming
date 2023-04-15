6a2bb82782b7854b003468042acd5d38af91c100
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-15 01:35:30 -0300

    add task 9

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -4,9 +4,13 @@
  * cada deccionario guarda <len de lista>:<value>
  * un for para printear ky:value
  */
+
+const list = [];
+
 exports.logMe = function (item) {
+  list.push(item);
+  console.log(list.length + ': ' + item);
+};
 
-}
-
-//ouput format
-//<number arg already printed>: <current arg value>
+// ouput format
+// <number arg already printed>: <current arg value>
