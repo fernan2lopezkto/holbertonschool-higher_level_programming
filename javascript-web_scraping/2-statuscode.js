cc46b0438dd992bfe4a219beab77bb4b2fe60244
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-17 09:58:58 -0700

    task 2 fix 3

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-  console.error('error:', error);
-  console.log('code:', response && response.statusCode);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });
