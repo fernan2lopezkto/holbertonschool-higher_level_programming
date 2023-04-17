9f8c384347c2ebc023f3aeeb701844089ba7851b
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-17 13:47:48 -0700

    stop to wirk

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// Write a script.
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    for ()
+    (JSON.parse(body).);
+  }
+});
