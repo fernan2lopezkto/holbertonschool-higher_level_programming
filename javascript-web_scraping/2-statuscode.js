b985aa9fbacc100a6a42e4efe6a77995b6baef2f
juan in sanvox <fernan2lopezkto@gmail.com>
2023-04-17 09:18:41 -0700

    try task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 // Write a script that display the status code of a GET request.
 
+const request = require('request');
+const url = process.argv[2];
+
+request (url, function (error, response, body) {
+    console.log('code: ', response && response.statusCode);
+});
