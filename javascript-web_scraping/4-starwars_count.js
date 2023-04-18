398f66aeb94d81991ed0ff60c293af6e1754d88e
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2023-04-17 22:22:42 -0300

    Actualizar 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -8,13 +8,13 @@ request(url, function (error, response, body) {
   if (error) {
     console.log(error)
   } else {
-    if (response.statusCode === 200) {
-      const data = JSON.parse(body).results;
-      let count = 0;
-      for (const each of data) {
-        for (const chars of each.characters) {
-          if (chars.endsWith('/18/')) {
-            ++count;
+    if (response.statusCode === 200)
+      const data = JSON.parse(body).results;
+      let count = 0;
+      for (const each of data) {
+        for (const chars of each.characters) {
+          if (chars.endsWith('/18/')) {
+          ++count;
           }
         }
       }
