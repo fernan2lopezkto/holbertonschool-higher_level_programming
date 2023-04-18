1670651dd77eb4588a3e3355f6ee269bb4753dfd
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-18 11:28:15 -0300

    stop yo brake

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// Write a script that computes the number
+// of tasks completed by user id.
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const page = JSON.parse(body);
+      const outputs = {}
+      for (const each of page) {
+        if (page.completed === true) {
+          outputs[page.userId] = 
+        }
+      }
+    }
+  });
