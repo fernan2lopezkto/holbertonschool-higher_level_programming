b94f0d66665c492e026213306f5d91159e440aef
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2023-04-17 23:08:12 -0300

    Actualizar semicolon

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
     if (response.statusCode === 200) {
       const data = JSON.parse(body).results;
